3c677122000b3cfdc6b96c4c324c8087dbc98b61
EdSpin <ertugrul@spinmatic.com>
2022-08-15 20:03:02 +0300

    Modified

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb;
+    SurfaceEffector2D surfaceEffector2D;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     private void Update()
+    {
+        RotatePlayer();
+        RespondToBost();
+    }
+
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -21,6 +31,22 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.AddTorque(-torqueAmount);
-        }                
+        }
+    }
+
+    private void RespondToBost()
+    {
+        // if we push uo, then speed up
+        // otherwise stay at normal speed
+        // access the surfaceeffector2d and change its speed
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
     }
 }
